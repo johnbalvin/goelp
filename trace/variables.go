1a9bbfc27355b69deb2f4ffb855ad94c833dc905
john <johnbalvin1994@gmail.com>
2024-01-13 17:20:28 -0500

    more descriptive blocked error

--- trace/variables.go
+++ trace/variables.go
@@ -6,7 +6,7 @@ import (
 
 var (
 	ErrEmpty      = errors.New("err Empty restult")
-	ErrPermisions = errors.New("err Yelp requested captcha, try with another IP")
+	ErrPermisions = errors.New("err Yelp blocked your IP, try with another IP")
 	ErrParameter  = errors.New("err not correct parameters")
 	ErrMaxAttempt = errors.New("err Max attemps")
 	ErrStatusCode = errors.New("err Not a correct status code")
